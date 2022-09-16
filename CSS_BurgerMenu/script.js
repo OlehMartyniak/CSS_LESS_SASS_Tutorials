339434a48d82a1d6d5f7832b949e77ea58bb41d3
OlehMartyniak <97449514+OlehMartyniak@users.noreply.github.com>
2022-09-16 10:44:45 +0300

    hamburger menu

--- CSS_BurgerMenu/script.js
+++ CSS_BurgerMenu/script.js
@@ -4,4 +4,9 @@ const navMenu = document.getElementsByClassName("nav-menu")[0];
 hamburger.addEventListener("click", ()=> {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", ()=> {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
